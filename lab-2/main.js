0ea660def566f28577bf9f41c18be4315f7c4e20
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2022-10-19 23:43:37 +0200

    .

--- lab-2/main.js
+++ lab-2/main.js
@@ -1,9 +1,20 @@
 
-const slides= document.querySelectorAll('.slides')
-slides.foreach((s,i)=>s.style.transform=
-`translateX(${100*i})`);
+const slides= document.querySelectorAll('.slide')
+const btnRight = document.querySelector('.slider_btn_right');
+const btnLeft = document.querySelector('.slider_btn_left');
+
+let slideNumber = 0;
+slides.foreach((s,i)=>(s.style.transform=
+`translateX(${100*i}%)`));
 
 const slider = document.querySelector('.slider')
+slider.style.transform = 'scale(0.1) translateX(-800px)'
+btnRight.addEventListener('click',function(){
+ slideNumber++;
+
+slides.foreach((s,i)=>(s.style.transform=
+`translateX(${100*(i-slideNumber)}%)`));
+})
 
 
 
@@ -13,29 +24,28 @@ const slider = document.querySelector('.slider')
 
 
 
-
-// notatnik z zajęć
-const main = document.querySelector('main')
-const timeoutRef = setTimeout(
-    () => {
-        main.innerHTML = 'From setTimeout'
-    },
-    2000
-)
-let licznik = 0
-const intervalRef = setInterval(
-    () => {
-        main.innerHTML = 'From interval' + licznik++
-    },
-    4000
-)
-
-
-// kasujemy setInterval
-clearInterval(intervalRef)
-
-// kasujemy setTimeout
-clearTimeout(intervalRef)
+// // notatnik z zajęć
+// const main = document.querySelector('main')
+// const timeoutRef = setTimeout(
+//     () => {
+//         main.innerHTML = 'From setTimeout'
+//     },
+//     2000
+// )
+// let licznik = 0
+// const intervalRef = setInterval(
+//     () => {
+//         main.innerHTML = 'From interval' + licznik++
+//     },
+//     4000
+// )
 
 
-// window.requestAnimationFrame
+// // kasujemy setInterval
+// clearInterval(intervalRef)
+
+// // kasujemy setTimeout
+// clearTimeout(intervalRef)
+
+
+// // window.requestAnimationFrame
