9f104a8697880d865fd8bc65e35ea45743f75568
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2022-10-21 00:18:00 +0200

    lab-2 done, lab-3

--- lab-2/main.js
+++ lab-2/main.js
@@ -6,16 +6,38 @@ const btnRight = document.querySelector('.slider_btn_right');
 const btnLeft = document.querySelector('.slider_btn_left');
 const slider = document.querySelector('.slider')
 // document.querySelector('body').style.backgroundColor='#222';
-
+const dots = document.querySelector('.dots');
 
 // slides.style.backgroundColor='#222';
+
+const newDot = function(){
+    slides.forEach(function(_,i){
+        dots.insertAdjacentHTML(
+        'beforeend',
+        `<button class="dots__dot" data-slide="${i}"></button>`);
+    });
+};
+newDot();
+
+const activateDots =function(slide){
+    document
+    .querySelectorAll('.dots__dot')
+    .forEach(dot=> dot.classList
+    .remove('dots__dot--active'));
+
+    document.querySelector(`.dots__dot[data-slide="${slide}"]`)
+    .classList.add('dots__dot--active');
+};
+activateDots(0);
+
 let slideNumber = 0;
 let amountSlide = slides.length;
 slides.forEach((s,i)=>(s.style.transform=
 `translateX(${100*i}%)`));
 
+
 slider.style.backgroundColor='#222';
-// slider.style.transform = 'scale(0.8) translateX(-800px)'
+
 btnRight.addEventListener('click',function(){
     if(slideNumber===amountSlide-1){
         slideNumber =0;
@@ -26,7 +48,7 @@ btnRight.addEventListener('click',function(){
     slides.forEach((s,i)=>(s.style.transform=
     `translateX(${100*(i-slideNumber)}%)`));
     
-
+     activateDots(slideNumber);
 });
 btnLeft.addEventListener('click',function(){
     if(slideNumber===0){
@@ -39,9 +61,17 @@ btnLeft.addEventListener('click',function(){
     slides.forEach((s,i)=>(s.style.transform=
     `translateX(${100*(i-slideNumber)}%)`));
     
-
+    activateDots(slideNumber);
 });
 
+dots.addEventListener('click',function(e){
+    if(e.target.classList.contains('dots__dot')){
+    const slide = e.target.dataset.slide;
+    slides.forEach((s,i)=>(s.style.transform=
+        `translateX(${100*(i-slide)}%)`));
+        activateDots(slide);
+    };
+});
 
 
 
