c27db2f09a9fa36fa46fe23ba6b2a4e582260daf
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2022-11-07 12:46:07 +0100

    notesViewJs set

--- lab-4/script.js
+++ lab-4/script.js
@@ -1,5 +1,25 @@
 "use strict"
 import NotesAPI from "./notesAPI.js"
+import NotesView from "./notesView.js"
 
-NotesAPI.deleteNote(741431)
-console.log(NotesAPI.getAllNotes());
+const app = document.getElementById('app');
+const view =new NotesView(app,{
+    onNoteAdd(){
+        console.log("add new notes");
+    },
+    onNoteSelect(id){
+        console.log("note selected:"+id);
+    },
+    onNoteDelete(id){
+        console.log("note deleted:"+id);
+    },
+    onNoteEdit(newTitle,newBody){
+        console.log(newTitle);
+        console.log(newBody);
+
+    },
+
+})
+const notes=NotesAPI.getAllNotes();
+view.updateNoteList(notes);
+view.updateActiveNote(notes[0])
