dfc45f3626362100516f52f06f0f07579a3ce5d2
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2022-11-10 14:54:39 +0100

    lab5

---
+++ lab-5/main.js
@@ -0,0 +1,34 @@
+const func =[saveCToSessionStorage,discoverPowerBallNumber];
+interval(func);
+function interval(func) {
+    func.forEach(f => {
+        let timer = 1
+        setInterval(
+          () => {
+            // mamy coupling - interval ma na sztywno zaszyte w sobie C i D (..i logger)
+            f(timer);
+            timer++
+          }
+          , 2000) 
+    });
+  
+}
+
+class Logger {
+  static log(data) {
+    console.log(data)
+  }
+}
+
+function saveCToSessionStorage(data) {
+  console.log('[reader C]', data)
+  const storageData = { data }
+  sessionStorage.setItem('C', JSON.stringify(storageData))
+  // brudzimy funkcję loggerem - to nie jest jej funkcjonalność!
+  Logger.log(`[log from C] ${data}`)
+}
+
+function discoverPowerBallNumber(data) {
+  const number = Math.floor(Math.random() * data * 100)
+  console.log('[powerball number]', number)
+}
