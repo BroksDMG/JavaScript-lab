f4140b4a67a84e1233f075781a55d497b7ae88b9
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2022-11-26 13:59:58 +0100

    cleaned code

--- lab-6/index.js
+++ lab-6/index.js
@@ -1,23 +1,19 @@
 'use strict';
 const main = document.querySelector('.main')
 const ball = document.querySelector('.ball');
-
-
-
 window.addEventListener('deviceorientation', changePosition);
-const circle =document.getElementById('circle');
 let speedX=0, speedY=0;
-let ballTop =parseInt(window.getComputedStyle(ball).getPropertyValue("top"));
+let ballTop =parseInt(window.getComputedStyle(ball).getPropertyValue("top"));       //ball position from top and left
 let ballleft =parseInt(window.getComputedStyle(ball).getPropertyValue("left"));
 
-function changePosition(e){
+function changePosition(e){  // ball speed
     speedX=e.gamma/5
     speedY=e.beta/5
 }
-function onDeviceMove() {
-    let boardBounds=main.getBoundingClientRect();
+function onDeviceMove() {       //moving ball
+    let boardBounds=main.getBoundingClientRect();       // borders of board and ball
     let ballBounds=ball.getBoundingClientRect();
-    if(ballBounds.top+speedX>boardBounds.top&&ballBounds.bottom+speedX<boardBounds.bottom){
+    if(ballBounds.top+speedX>boardBounds.top&&ballBounds.bottom+speedX<boardBounds.bottom){   //limits for the ball
         ballTop+=speedX;
         ball.style.top=ballTop+'px';
     }
@@ -25,7 +21,6 @@ function onDeviceMove() {
         ballleft+=speedY;
         ball.style.left=ballleft+'px';
     }
- 
 window.requestAnimationFrame(onDeviceMove)
 }
 onDeviceMove();
@@ -33,87 +28,3 @@ onDeviceMove();
 
 
 
-// function onDeviceMove(event) {
-//     let leftright= event.beta/100
-//     let right= leftright?true:false
-//     let updonw= event.gamma
-//     let up= updonw?true:false
-//     let ballMove=setInterval(()=>{
-//         let ballBounds=ball.getBoundingClientRect();
-//         let boardBounds=main.getBoundingClientRect();
-//         let ballBoundsLeft=parseInt(ballBounds.left);
-//         let ballBoundsRight=parseInt(ballBounds.right);
-//         let ballBoundsTop=parseInt(ballBounds.top);
-//         let ballBoundsBottom=parseInt(ballBounds.bottom);
-//         let ballTop=parseInt(window.getComputedStyle(ball).getPropertyValue("top"))
-//         let ballLeft=parseInt(window.getComputedStyle(ball).getPropertyValue("left"))
-//         console.log(ballTop);
-//         if(right&&up){
-//             ball.style.top=ballTop-velocity+"px"
-//             ball.style.left=ballLeft+velocity+"px"
-//         }
-//         if(!right&&up){
-//             ball.style.top=ballTop-velocity+"px"
-//             ball.style.left=ballLeft-velocity+"px"
-//         }
-//         if(right&&!up){
-//             ball.style.top=ballTop+velocity+"px"
-//             ball.style.left=ballLeft+velocity+"px"
-//         }
-//         if(!right&&!up){
-//             ball.style.top=ballTop+velocity+"px"
-//             ball.style.left=ballLeft-velocity+"px"
-//         }
-//         if(ballBoundsTop<=boardBounds.top){
-//             leftright= event.beta/100
-//             right= leftright?true:false
-//             up=false;
-//         }
-//         if(ballBoundsBottom>=boardBounds.bottom){
-//             leftright= event.beta/100
-//             right= leftright?true:false
-//             up=true;
-//         }
-//         if(ballBoundsRight>=boardBounds.right){
-//             right=false
-//             updonw= event.gamma/100
-//             up= updonw?true:false
-//         }
-//         if(ballBoundsLeft<=boardBounds.left){
-//             right=true
-//             updonw= event.gamma/100
-//             up= updonw?true:false
-//         }
-        
-//     },10)
-// }
-
-function animate() {
-    //    console.log(Date.now())
-    // requestAnimationFrame(animate)
-}
-
-requestAnimationFrame(animate)
-
-
-    // const arows =document.addEventListener('keydown', event=>{
-    //     if(both===0){
-    //     both++;
-    //     console.log(both);
-    //     if(event.key==="ArrowLeft"){
-    //         interval =setInterval(moveLeft,1)
-    //     }
-    //     if(event.key==="ArrowRight"){
-    //         interval=setInterval(moveRight,1)
-    //     }
-    //     if(event.key==="ArrowUp"){
-    //         interval =setInterval(moveUp,1)
-    //     }
-    //     if(event.key==="ArrowDown"){
-    //         interval=setInterval(moveDown,1)
-    //     }}
-    // })
-    // document.addEventListener('keyup',event=>{
-    //     clearInterval(interval);
-    //     both=0;
-    // })
