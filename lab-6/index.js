a9556d6e1c030c7f3ff79ee40f6ea8d6e173df8f
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2022-11-27 16:57:50 +0100

    moving holes

--- lab-6/index.js
+++ lab-6/index.js
@@ -5,9 +5,14 @@ const startButton =document.querySelector('.start_button');
 window.addEventListener('deviceorientation', changePosition);
 let speedX=0, speedY=0;
 let result
+let leftright=Math.floor(Math.random()*2);
+let updonw= Math.floor(Math.random()*2);
+let right=leftright?true:false
+let up =updonw?true:false
+let velocity=3;
 let counter=0;
 let holesarr=[];
-let testarr=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+let score=[]
 let boardBounds;    
 let ballBounds;
 let ballTop =parseInt(window.getComputedStyle(ball).getPropertyValue("top"));       //ball position from top and left
@@ -30,10 +35,61 @@ function onDeviceMove() {       //moving ball
         ball.style.left=ballleft+'px';
     }
     createHole();
-
-
-
+    movehole();
 window.requestAnimationFrame(onDeviceMove)
+}
+
+function movehole(){
+    
+    if(counter<=20){
+        boardBounds=main.getBoundingClientRect(); 
+    let curhole= document.getElementById(`hole${counter}`)
+    let curholeBound=curhole.getBoundingClientRect();
+    let holeTop=parseInt(window.getComputedStyle(curhole).getPropertyValue("top"))
+    let holeLeft=parseInt(window.getComputedStyle(curhole).getPropertyValue("left"))
+    
+    if(right&&up){
+        console.log(curhole);  
+        curhole.style.top=holeTop-velocity+"px"
+        curhole.style.left=holeLeft+velocity+"px"
+    }
+    if(!right&&up){
+        console.log(curhole);  
+        curhole.style.top=holeTop-velocity+"px"
+        curhole.style.left=holeLeft-velocity+"px"
+    }
+    if(right&&!up){
+        console.log(curhole);
+        curhole.style.top=holeTop+velocity+"px"
+        curhole.style.left=holeLeft+velocity+"px"
+    }
+    if(!right&&!up){
+        console.log(curhole);
+        curhole.style.top=holeTop+velocity+"px"
+        curhole.style.left=holeLeft-velocity+"px"
+    }
+    if(curholeBound.top<=boardBounds.top){
+        leftright= Math.floor(Math.random()*2)
+        right= leftright?true:false
+        up=false;
+    }
+    if(curholeBound.bottom>=boardBounds.bottom){
+        leftright= Math.floor(Math.random()*2)
+        right= leftright?true:false
+        up=true;
+    }
+    if(curholeBound.right>=boardBounds.right){
+        right=false
+        updonw= Math.floor(Math.random()*2)
+        up= updonw?true:false
+    }
+    if(curholeBound.left<=boardBounds.left){
+        right=true
+        updonw= Math.floor(Math.random()*2)
+        up= updonw?true:false
+    }  
+    
+}
 }
 function createHole(){
     counter++;
@@ -42,15 +98,12 @@ function createHole(){
     hole.setAttribute('class','hole');
     hole.setAttribute('id','hole'+counter);
     main.appendChild(hole);
-    //////////moving
-    let curhole= document.getElementById(`hole${counter}`)
-    let curholeBound=curhole.getBoundingClientRect();
-    ///////////
     let randomTop =Math.floor(Math.random()*1000);
     let randomLeft =Math.floor(Math.random()*500);
     hole.style.top=randomTop+'px';
     hole.style.left=randomLeft+'px';
     holesarr.push(counter)
+    score.push(counter)
 }
 for(let i=0; i<holesarr.length;i++){
     let current =holesarr[i];
@@ -62,8 +115,8 @@ for(let i=0; i<holesarr.length;i++){
     result=dx*dx+dy*dy;
     if(result<900){
         currHole.classList.add('hidden_hole');
-        testarr.pop();
-        console.log(testarr); 
+        score.pop();
+        console.log(score); 
     }
 
 }
