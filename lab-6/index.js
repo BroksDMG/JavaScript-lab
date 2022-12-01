1ccf15c86a90bdff1bef9d83fefdf1fd02edd6e8
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2022-12-01 14:50:26 +0100

    adding time

--- lab-6/index.js
+++ lab-6/index.js
@@ -9,7 +9,7 @@ let speedX = 0,speedY = 0;
 let someBlackHoles = [];
 let counter = 0;
 let holesarr = [];
-let score = [];
+let score =0;
 let boardBounds = main.getBoundingClientRect();
 let ballBounds = ball.getBoundingClientRect();
 let curhole;
@@ -17,6 +17,10 @@ let curholeBound;
 let ballTop = parseInt(window.getComputedStyle(ball).getPropertyValue("top")); //ball position from top and left
 let ballleft = parseInt(window.getComputedStyle(ball).getPropertyValue("left"));
 let gameStart = false;
+let startTime
+let endTime
+let gameEnd=false;
+let scoreArr=[];
 function changePosition(e) {        // ball speed
   speedX = e.gamma / 30;
   speedY = -e.beta / 30;
@@ -48,6 +52,7 @@ class Hole {
       }
     }
   }
+  
   movehole() {
     boardBounds = main.getBoundingClientRect();
     curhole = document.getElementById("hole" + this.id);
@@ -75,6 +80,8 @@ function Holes() {
     holes[i].movehole();
   }
   for (let i = 0; i < holesarr.length; i++) {
+    
+    
     ballBounds = ball.getBoundingClientRect();
     let current = holesarr[i];
     let currHole = document.getElementById("hole" + current);
@@ -87,7 +94,7 @@ function Holes() {
     let sumOfRays=ballR*ballR+holeR*holeR
     if (distanse < sumOfRays) {
       currHole.classList.add("hidden");
-      score.pop();
+      score++;
       console.log(score);
       if (currHole.classList.contains("black_hole")) {
         ballleft = random(ballBounds.width, boardBounds.width - ballBounds.width * 2);   //holr left/right
@@ -95,19 +102,29 @@ function Holes() {
         ball.style.top = ballTop + "px";
         ball.style.left = ballleft + "px";
       }
+      
     }
+    
   }
 }
 function onDeviceMove() {
   moveBall()
   Holes();
+  if(!(score>2)){
   window.requestAnimationFrame(onDeviceMove);
+  }else
+  {
+    endTime=new Date().getTime();
+    console.log(endTime-startTime);
+  }
 }
 startButton.addEventListener("click", function () {     ///start game
   startButton.classList.toggle("active_start");
   if (startButton.classList.contains("active_start")) {
     gameStart = true;
     onDeviceMove();
+    startTime= new Date().getTime();
+    console.log(startTime)
   }
   createHoles();
 });
@@ -136,7 +153,6 @@ function createHoles(){
         counter++;
         if (counter < 5) someBlackHoles.push(random(0, 20));
         holesarr.push(counter);
-        score.push(counter);
         let hole = new Hole(
           counter, //id
           random(ballBounds.height, boardBounds.height - ballBounds.height * 2), //position y // top bottom
