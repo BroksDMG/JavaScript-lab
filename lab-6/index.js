cc1f1cf805955b92e13b6bdd8178d8c4d0350140
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2022-12-03 18:30:33 +0100

    time aded

--- lab-6/index.js
+++ lab-6/index.js
@@ -3,8 +3,15 @@ window.addEventListener("deviceorientation", changePosition);
 const main = document.querySelector(".main");
 const ball = document.querySelector(".ball");
 const body = document.querySelector("body");
+const timeConteiner =document.querySelector(".time_conteiner")
+const restarConteiner =document.querySelector(".restart_conteiner")
+const timeText=document.querySelector(".time_text")
+const minutes =document.querySelector(".time_minutes");
+const seconds =document.querySelector(".time_seconds");
 const startButton = document.querySelector(".start_button");
-const holes = [];
+const restartButton =document.querySelector(".restart_button")
+restartButton.addEventListener("click",restar)
+let holes = [];
 let speedX = 0,speedY = 0;
 let someBlackHoles = [];
 let counter = 0;
@@ -21,6 +28,9 @@ let startTime
 let endTime
 let gameEnd=false;
 let scoreArr=[];
+let ballquantity=2;
+let resSec =0,resMin=0;
+let currentHoleBound;
 function changePosition(e) {        // ball speed
   speedX = e.gamma / 30;
   speedY = -e.beta / 30;
@@ -56,6 +66,7 @@ class Hole {
   movehole() {
     boardBounds = main.getBoundingClientRect();
     curhole = document.getElementById("hole" + this.id);
+    if(!(curhole===null)){
     curholeBound = curhole.getBoundingClientRect();
     if (
       curholeBound.right + speedX*2 >= boardBounds.right ||
@@ -73,6 +84,7 @@ class Hole {
     this.y += this.velY;
     curhole.style.top = this.x + "px";
     curhole.style.left = this.y + "px";
+    }
   }
 }
 function Holes() {
@@ -80,12 +92,12 @@ function Holes() {
     holes[i].movehole();
   }
   for (let i = 0; i < holesarr.length; i++) {
-    
-    
     ballBounds = ball.getBoundingClientRect();
     let current = holesarr[i];
     let currHole = document.getElementById("hole" + current);
-    let currentHoleBound = currHole.getBoundingClientRect();
+    if(!(currHole===null)){
+        currentHoleBound = currHole.getBoundingClientRect();
+    
     let ballR=ballBounds.right-ballBounds.x
     let holeR=currentHoleBound.right-currentHoleBound.x
     let dx = currentHoleBound.x - ballBounds.x;
@@ -95,7 +107,6 @@ function Holes() {
     if (distanse < sumOfRays) {
       currHole.classList.add("hidden");
       score++;
-      console.log(score);
       if (currHole.classList.contains("black_hole")) {
         ballleft = random(ballBounds.width, boardBounds.width - ballBounds.width * 2);   //holr left/right
         ballTop = random(ballBounds.height, boardBounds.height - ballBounds.height * 2); //hole top/bottom
@@ -104,30 +115,70 @@ function Holes() {
       }
       
     }
+    }
     
   }
 }
-function onDeviceMove() {
+function Animation() {
   moveBall()
   Holes();
-  if(!(score>2)){
-  window.requestAnimationFrame(onDeviceMove);
+  if(!(score>=ballquantity)){
+  window.requestAnimationFrame(Animation);
   }else
   {
-    endTime=new Date().getTime();
-    console.log(endTime-startTime);
+    endGame();
   }
 }
-startButton.addEventListener("click", function () {     ///start game
-  startButton.classList.toggle("active_start");
-  if (startButton.classList.contains("active_start")) {
-    gameStart = true;
-    onDeviceMove();
-    startTime= new Date().getTime();
-    console.log(startTime)
+function endGame(){
+    timeConteiner.classList.remove("hidden");
+    restarConteiner.classList.remove("hidden");
+    timeText.classList.remove("hidden");
+    ball.classList.add("hidden");
+    time()
+    counter=0;
+    score=0;
+    speedX=0;
+    speedY=0;
+    for (let i = 1; i < holes.length+1; i++) {
+        document.getElementById("hole"+i).remove()
+      }
+      holes=[];
+    ball.style.top=50+"%"
+    ball.style.left=50+"%";
+}
+function restar(){
+    timeConteiner.classList.add("hidden");
+    restarConteiner.classList.add("hidden");
+    timeText.classList.add("hidden");  
+    startButton.classList.remove("active_start")
+    ball.classList.remove("hidden");
+    // startTime= new Date().getTime();
+    // Animation();
+    // createHoles();
+}
+function time(){
+    endTime=new Date().getTime();
+    let second =Math.floor(((endTime-startTime)/1000)-resSec);
+    let minute=Math.floor(((endTime-startTime)/60000)-resMin);
+    if(second>=60)resSec+=60
+    if(minute>=60)resMin+=60
+    seconds.textContent=second.toString().padStart(2,"0")+"s";
+    minutes.textContent=minute.toString().padStart(2,"0");
+}
+startButton.addEventListener("click", start);
+function start() {     ///start game button
+    if(!(startButton.classList.contains("active_start"))){
+      startButton.classList.add("active_start");
+    }
+    if (startButton.classList.contains("active_start")) {
+      ballTop = parseInt(window.getComputedStyle(ball).getPropertyValue("top")); //ball position from top and left
+      ballleft = parseInt(window.getComputedStyle(ball).getPropertyValue("left"))
+      gameStart = true;
+      createHoles();
+      Animation();
+      startTime= new Date().getTime();
+    }
   }
-  createHoles();
-});
 function moveBall(){
     //moving ball
   boardBounds = main.getBoundingClientRect(); // borders of board and ball
@@ -149,9 +200,9 @@ function moveBall(){
   }
 }
 function createHoles(){
-    while (holes.length < 20) {
+    while (holes.length < ballquantity) {
         counter++;
-        if (counter < 5) someBlackHoles.push(random(0, 20));
+        if (counter < 5) someBlackHoles.push(random(0, ballquantity));
         holesarr.push(counter);
         let hole = new Hole(
           counter, //id
