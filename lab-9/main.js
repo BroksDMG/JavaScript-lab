49aa1f8570742b51c87854d3c4b3c7aff96c5456
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2023-01-02 18:05:17 +0100

    .

--- lab-9/main.js
+++ lab-9/main.js
@@ -1,10 +1,16 @@
 "use strict";
-let setup = {
-    apiKey: function(){},
-    fetchWeather:function(){
-        fetch().then
-    }
+// let setup = {
+//     apiKey: function(){},
+//     fetchWeather:function(){
+//         fetch().then
+//     }
+// }
+
+function setupp(){
+    loadJSON('https://api.openweathermap.org/data/2.5/weather?q=Warsaw&appid=e405c2dbb78e5a79771cb55742d2ade1&units=metric',gotData)
+    console.log(loadJSON);
 }
 function gotData(data){
-    weather =data;
+    let weather=data;
+    console.log(weather);
 }
