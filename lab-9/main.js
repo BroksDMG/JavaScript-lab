c3e02904a9038d1bfd65a47ef8f12f9f1d107417
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2023-01-03 13:59:14 +0100

    founding city

--- lab-9/main.js
+++ lab-9/main.js
@@ -1,16 +1,17 @@
 "use strict";
-// let setup = {
-//     apiKey: function(){},
-//     fetchWeather:function(){
-//         fetch().then
-//     }
-// }
+const searchBTN = document.querySelector('.search-btn')
+const inputText =document.querySelector('.search-bar');
+const apiKey ='e405c2dbb78e5a79771cb55742d2ade1';
 
-function setupp(){
-    loadJSON('https://api.openweathermap.org/data/2.5/weather?q=Warsaw&appid=e405c2dbb78e5a79771cb55742d2ade1&units=metric',gotData)
-    console.log(loadJSON);
-}
-function gotData(data){
-    let weather=data;
-    console.log(weather);
-}
+
+searchBTN.addEventListener('click',function(){
+
+
+    const cityInput =inputText.value;
+    console.log(cityInput);
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}&units=metric`)
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data);
+    })
+});
