1a9698bd96f1bc71efe866e8c579c29ff8278fc4
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2023-01-15 14:02:13 +0100

    adding saveing etc

--- lab-4/notekeeper.js
+++ lab-4/notekeeper.js
@@ -1,2 +1,32 @@
 const notesContainer =document.getElementById('app')
-console.log(notesContainer);
+const addBtn=document.querySelector('.notes__add')
+
+function getNotes(){
+    return JSON.parse(localStorage.getItem("notes-container")||"[]");
+}
+
+function saveNotes(notes){
+    localStorage.setItem("notes-container",JSON.stringify(notes))
+}
+function createNoteElement(id,content){
+    const elemtntNoteList=document.createElement('div')
+
+    elemtntNoteList.classList.add('notes__list-item');
+    elemtntNoteList.value=content;
+    elemtntNoteList.placeHolder="notes__lsit"
+
+    elemtntNoteList.addEventListener("change",function(){
+        updateNote(id,Element.value);
+    })
+
+    elemtntNoteList.addEventListener("dblclick",function(){
+
+    })
+
+    return elemtntNoteList
+
+}
+
+function updateNote(id,content){
+
+}
